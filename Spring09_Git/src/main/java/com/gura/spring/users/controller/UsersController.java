474c9dc225c94fb04a1b169ec835ca548f1446ec
blanc <code_blanc@naver.com>
2017-01-20 11:30:18 +0900

    userscontroller 수정

--- Spring09_Git/src/main/java/com/gura/spring/users/controller/UsersController.java
+++ Spring09_Git/src/main/java/com/gura/spring/users/controller/UsersController.java
@@ -2,37 +2,70 @@ package com.gura.spring.users.controller;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.gura.spring.users.dto.UsersDto;
 import com.gura.spring.users.service.UsersService;
 
-//component 스캔시 bean 이 되고 또한 컨트롤러 역할을 할 수 있도록
+// component 스켄시 bean 이되고 또한 컨트롤러 역활을 할수 있도록
 @Controller
 public class UsersController {
-	//의존 객체 주입 되도록
+	//의존 객체 주입 되도록 
 	@Autowired
 	private UsersService usersService;
 	
-	//2
-	//ajax "/users/checkid.do 요청 처리
-	@RequestMapping("/users/checkid")
-	public Map<String, Object> checkid(){
-		return null;
+	// "/users/signup.do" 요청처리
+	@RequestMapping("/users/signup")
+	public ModelAndView signup(HttpServletRequest request,
+			@ModelAttribute UsersDto dto){
+		//서비스를 이용해서 회원정보를 저장한다.
+		usersService.insert(dto);
+		ModelAndView mView=new ModelAndView();
+		mView.addObject("msg", dto.getId()+" 회원님 가입되었습니다.");
+		mView.addObject("redirectUri", request.getContextPath());
+		mView.setViewName("users/alert");
+		return mView;
 	}
-	//1 
+	
+	// ajax "/users/checkid.do" 요청 처리
+	@RequestMapping("/users/checkid")
+	@ResponseBody
+	public Map<String, Object> checkid(@RequestParam String inputId){
+		Map<String, Object> map=usersService.canUseId(inputId);
+		// json 문자열 응답하기 
+		return map;
+	}
+	
 	@RequestMapping("/users/signup_form")
 	public String signupForm(){
 		
 		return "users/signup_form";
 	}
-	
-	// sign up 요청처리
-	@RequestMapping("/users/signup")
-	public String signup(){
-		
-		return "users/signup";
-	}
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
